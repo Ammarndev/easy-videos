043b0fb2cbcb692bb42fc5e0ee11b5e13e4929a0
Ammarndev <ammarn.dev@gmail.com>
2022-01-19 18:30:04 +0500

    getting video category id via api against each video id and then get category name against each categpry id via api and store in database and also check category already exist or not

--- video_importer/import_videos.php
+++ video_importer/import_videos.php
@@ -67,6 +67,9 @@ if(isset($_REQUEST)) {
                         'post_status'   =>  'publish', 
                         'post_content'  =>  $video_id
                     ));
+
+                    // RETRIEVE CATEGORY NAME AND INSERTION OF CATEGORY
+                    processPost($categoryId, $new_post, $apiKey);
                 }
             }
         }
@@ -81,5 +84,74 @@ if(isset($_REQUEST)) {
     }
 }
 
+// CATEGORY NAME API
+function processPost($category_id, $video_id, $apiKey) {
+    if($category_id != '') {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL             => 'https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&id='. $category_id .'&key=' . $apiKey,
+        CURLOPT_RETURNTRANSFER  => true,
+        CURLOPT_ENCODING        => "",
+        CURLOPT_MAXREDIRS       => 10,
+        CURLOPT_TIMEOUT         => 30,
+        CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST   => "GET",
+        CURLOPT_HTTPHEADER      => array(
+            "cache-control: no-cache",
+            "postman-token: 5ef4ffed-48c8-c2f7-560e-d1e6bb00c05b"
+            ),
+        ));
 
+        $response = curl_exec($curl);   // API EXECUTE
+        curl_close($curl);              // API REQUEST CLOSE
+        $res = json_decode($response);  // API RESPONSE DECODE
+
+        // CHECK RESPONSE
+        if($res) {
+            // GETTING CATEGORY NAME IN API RESPONSE
+            foreach($res->items as $item) {
+                $category_id     = $item->id;
+                $category_name   = $item->snippet->title;
+
+                // GETTING ALL CATEGORIES
+                $categories = get_terms([
+                    'taxonomy' => 'vid_cat',
+                    'hide_empty' => false,
+                ]);
+
+                $found  = 0;
+                $cat_id = 0;
+
+                // CHECK IF CATEGORY ALREADY EXIST
+                foreach( $categories as $category ) {
+                    if(trim($category->description) == $category_id) {
+                        $found  = 1;
+                        $cat_id = $category->term_id;
+                    }
+                }
+
+                if($found) {
+                    // set category to new post
+                    wp_set_post_terms( $video_id, $cat_id, 'vid_cat');
+
+                } else {
+                    require_once( ABSPATH . '/wp-admin/includes/taxonomy.php');
+                    $category = array(
+                        'cat_name'              => $category_name,
+                        'category_description'  => $category_id,
+                        'category_nicename'     => $category_name,
+                        'category_parent'       => '',
+                        'taxonomy'              => 'vid_cat'
+                    );
+
+                    // Create the category
+                    $new_category = wp_insert_category($category);
+
+                    // set category to new post
+                    wp_set_post_terms( $video_id, $new_category, 'vid_cat');
+                }
+            }
+        }
+    }
+}
 ?>
