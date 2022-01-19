5852d8552e4a33035b0e629950246088abdb2d3a
Ahmad Saleem <ahmad.saleem@akdesigner.com>
2022-01-19 17:58:58 +0500

    plugin creation of easy videos

---
+++ video_importer/video_importer.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Easy Videos
+
+Description: This plugin is made for client against there custom requirements which import youtube videos to wordpress database.
+
+Author: Ammar
+
+Text Domain: easyvideos
+*/
+
+?>
