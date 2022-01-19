fc23d7a1254d2ca65f5e353d7dfe39725caf31ad
Ammarndev <ammarn.dev@gmail.com>
2022-01-19 18:17:42 +0500

    include easy video plugin in side menu bar and create file of plugin view

--- video_importer/video_importer.php
+++ video_importer/video_importer.php
@@ -9,4 +9,28 @@ Author: Ammar
 Text Domain: easyvideos
 */
 
+// Pluign activation and deactivation hooks
+register_activation_hook(__FILE__, 'video_importer_activation');
+register_deactivation_hook(__FILE__, 'video_importer_deactivation');
+
+// Pluign activation function
+function video_importer_activation() {
+}
+
+// Pluign deactivation function
+function video_importer_deactivation() {
+}
+
+// Add plugin in side menu bar
+add_action('admin_menu', 'video_importer_menu');
+
+// Add plugin in side menu bar function
+function video_importer_menu() {
+    add_menu_page('Easy Videos','Easy Videos Options', 'administrator', __FILE__, 'video_page');
+}
+
+// Include file
+function video_page() {
+    include( plugin_dir_path( __FILE__ ) . 'welcome.php');
+}
 ?>
